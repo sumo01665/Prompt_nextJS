f6fec4cb146c64e0c8b0854a04e09308fc3ef17d
Trist T <t35truon@uwaterloo.ca>
2023-07-30 12:39:40 -0400

    Promptopia Summary
    Create Promptopia webpage on github

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import '@styles/globals.css';
+
+// file importation
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "Promptopia",
+    description: 'Discover & Share AI Prompts'
+}
+
+const RootLayout = ({ children, }) => {
+  return (
+    <html lang="en">
+        <div className="main">
+            <div className="gradient"></div>
+        </div>
+
+        <main className="app">
+            <Nav /> 
+            {children}
+        </main>
+    </html>
+
+  )
+}
+
+export default RootLayout
---
+++ app/page.jsx
@@ -0,0 +1,30 @@
+// import React from 'react'
+
+// ? File system
+import Feed from '@components/Feed';
+import Form from '@components/Form';
+
+const Home = () => {
+  return (
+//   Header
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          {" "}
+          AI-Powered Prompts{" "}
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts.
+      </p>
+
+      <Feed />
+      {/* <Nav /> */}
+    </section>
+  );
+};
+
+export default Home;
